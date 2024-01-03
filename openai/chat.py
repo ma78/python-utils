4b25f47e7aacc382502ff11e139455a2b9c552f5
Adam Wilmore <adam@mx51.io>
2024-01-04 09:26:49 +1100

    Added colours and formatting

--- openai/chat.py
+++ openai/chat.py
@@ -24,17 +24,17 @@ def main():
     )
 
     # Create assistant
-    assistant = client.beta.assistants.create(model="gpt-3.5-turbo-1106", name="Ooze", instructions="Be as helpful as possible")
+    assistant = client.beta.assistants.create(model="gpt-4", name="Ooze", instructions="Be as helpful as possible")
 
     # Create a thread
     thread = client.beta.threads.create()
 
     # Start
-    print(" * GPT: Ask me stuff. Enter 'done' when finished ...\n")
+    print_as_gpt("Ask me stuff. Enter 'done' when finished ...\n")
 
     # Create run
     while True:
-        message = input(" * You: ")
+        message = input("\033[93m * You: \033[0m")
         print()
 
         # Check if finished
@@ -51,22 +51,23 @@ def main():
         message = wait_for(client, thread.id, run.id)
 
         # Display message
-        print("\n * GPT: " + message + "\n")
+        print("\n")
+        print_as_gpt(message + "\n")
 
     # Loop finished. Clean up
     client.beta.assistants.delete(assistant.id)
-    print(" * GPT: Get outta here.\n")
+    print_as_gpt("Get outta here.\n")
 
 
 def wait_for(client, thread_id, run_id):
-    print("( thinking ", end="")
+    print("\033[92m( thinking ", end="")
 
     while True:
         run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
 
         if run.status == "completed":
             # Finish thinking bubble
-            print(" )", flush=True)
+            print(" )\033[0m", flush=True)
 
             # Get only latest message
             messages = client.beta.threads.messages.list(thread_id=thread_id, limit=1)
@@ -75,6 +76,12 @@ def wait_for(client, thread_id, run_id):
         else:
             print(".", end="", flush=True)
             time.sleep(1)
+
+
+# Print with green
+def print_as_gpt(text):
+    """Print with green"""
+    print("\033[92m * GPT:\033[0m", text)
 
 
 # pylint: disable=W0613
